131a2fe51ef2cce61dc8a639a293a27b2236472b
Pavel Salnikov <flitvious@users.noreply.github.com>
2018-08-31 16:59:56 +0300

    Russian language support for Sphinx

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'ru'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -167,6 +167,7 @@ texinfo_documents = [
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 todo_emit_warnings = False
+
 
 # -- Prolog substitutions (available on all pages)
 
